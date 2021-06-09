ecf0742fcba59b3a90af7231be11a1416e1e35e6
Gurleen28 <gk28@outlook.it>
2021-06-09 20:46:27 +0200

    Hallo

--- FamilyTree/main.cpp
+++ FamilyTree/main.cpp
@@ -7,5 +7,6 @@ int main(int argc, char *argv[])
     QApplication a(argc, argv);
     MainWindow w;
     w.show();
+    std::cout << "Liebe Gruesse von GUrleen" <<std::endl;
     return a.exec();
 }
