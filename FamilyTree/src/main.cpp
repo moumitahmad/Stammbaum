3ec157768f95bd88e13cb0f6e3164dac8b49ef29
moumitahmad <moumita.ahmad@gmail.com>
2021-07-10 18:44:03 +0200

    fix merdge

--- FamilyTree/src/main.cpp
+++ FamilyTree/src/main.cpp
@@ -14,18 +14,9 @@ int main(int argc, char *argv[])
     database::IDatabase* pDB = new database::IDatabase();
     domain::ILogic* pLogic = new domain::ILogic(pDB);
     pLogic->connectToDatabase();
-<<<<<<< HEAD
 
     //MainWindow mw(pLogic);
     UserWindow uw(pLogic);
     uw.show();
-=======
-    testDatabase(pLogic);
-    //fillDatabase(pLogic);
-    QApplication a(argc, argv);
-    ApplicationWindow* app;
-    MainWindow w(pLogic, app);
-    w.show();
->>>>>>> c86b16cc78449c6af643b909899119ac01f2f116
     return a.exec();
 }
