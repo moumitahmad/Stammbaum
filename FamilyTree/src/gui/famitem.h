e60ddb8989904c4831cdc4f84855c252f146c4f3
moumitahmad <moumita.ahmad@gmail.com>
2021-07-18 15:18:59 +0200

    familyitem

--- FamilyTree/src/gui/famitem.h
+++ FamilyTree/src/gui/famitem.h
@@ -5,13 +5,18 @@
 #include <QPainter>
 #include "./value-objects/member.h"
 
-class famItem : public QGraphicsItem {
+
+// ----------------- ITEM ------------------
+class itemPart : public QGraphicsItem {
 
 public:
-    famItem(int xPos, int yPos, int width, int height, Member* member);
+    itemPart(int xPos, int yPos, int width, int height, Member* member, QImage image = QImage());
 
     QRectF boundingRect() const;
     void paint(QPainter *painter, const QStyleOptionGraphicsItem *option, QWidget *widget);
+    void setPressed(const bool& pressed);
+
+private:
     bool Pressed;
 
     int m_xPos;
@@ -19,9 +24,32 @@ public:
     int m_heigth;
     int m_width;
     Member* m_member;
+    QImage m_image;
+
+};
+
+
+
+// ------------------- GROUP ----------------
+class famItem : public QGraphicsItemGroup {
+
+public:
+    famItem(int xPos, int yPos, int width, int height, Member* member);
+private:
+    bool Pressed;
+    int m_xPos;
+    int m_yPos;
+    int m_heigth;
+    int m_width;
+    Member* m_member;
+    QImage m_image;
+    itemPart* m_imageItem;
+    itemPart* m_item;
+
 protected:
     void mousePressEvent(QGraphicsSceneMouseEvent *event);
 
 };
+
 
 #endif // FAMITEM_H
